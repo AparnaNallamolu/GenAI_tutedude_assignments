3361264e86892432427200e81338ff16b7bb4029
AparnaNallamolu <78528849+AparnaNallamolu@users.noreply.github.com>
2026-02-11 13:46:35 +0530

    Delete string_utils.py

--- string_utils.py
+++
@@ -1,9 +0,0 @@
-def capitalize_words(text):
-    return text.capitalize()
-
-def reverse_string(text):
-    reversed_text = text[::-1]
-    return reversed_text
-
-def word_count(text):
-    return len(text)
